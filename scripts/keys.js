2a55851db9ce67417e96c01a0c8961e59f4beb92
Evgeniy Polyakov <zbr@ioremap.net>
2016-06-28 02:06:36 +0300

    keys: download/embed url depending on the filename suffix

--- scripts/keys.js
+++ scripts/keys.js
@@ -1,9 +1,25 @@
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
 var KeyInfo = React.createClass({
   render: function() {
-    var url = this.props.get_url + "/" + this.props.bucket + "/" + this.props.xkey;
-    return (
-      <div className="keyInfo">
+    var url;
+    var data;
+    if (endsWith(this.props.filename, "(meta)")) {
+      url = this.props.get_key_url + "/" + this.props.bucket + "/" + this.props.xkey;
+      data = <div>
+        File: <a href={url} download={this.props.filename} target="_blank">{this.props.filename}</a>, Bucket: {this.props.bucket}
+      </div>
+    } else {
+      url = this.props.get_url + "/" + this.props.bucket + "/" + this.props.filename;
+      data = <div>
         File: <a href={url} target="_blank">{this.props.filename}</a>, Bucket: {this.props.bucket}
+      </div>
+    }
+    return (
+      <div class="keyInfo">
+        {data}
       </div>
     );
   }
